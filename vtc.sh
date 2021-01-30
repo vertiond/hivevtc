ecd39601b36997a36595f902ed147b1344f5edff
vertiond <35286924+vertiond@users.noreply.github.com>
2021-01-30 17:30:44 -0600

    Clean up

--- vtc.sh
+++ vtc.sh
@@ -12,6 +12,6 @@ miner stop
 
 chmod +x VerthashMiner                                                                                                                                                                                             
 
-./VerthashMiner -g conig.conf                                                                                                                                                                                      
+./VerthashMiner -g config.conf                                                                                                                                                                                      
 
 screen -dmS miner ./VerthashMiner -u vtc1qg9lkhlpcwfm43q73lp7m47qxy8v0cus67zwc6f.example -p x -o stratum+tcp://p2proxy.vertcoin.org:9175 --verthash-data verthash.dat --all-cl-devices --all-cu-devices

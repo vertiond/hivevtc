7b129260d85c96341c1ec50d8178f87119b15fb4
vertion <35286924+vertiond@users.noreply.github.com>
2021-01-30 23:02:24 -0600

    Replace original archive with one that is glibc 2.27 compatible
    Compiled on Ubuntu 18.04

--- vtc.sh
+++ vtc.sh
@@ -4,7 +4,7 @@ cd vtc
 
 wget https://vtconline.org/downloads/verthash.dat
 
-wget https://github.com/CryptoGraphics/VerthashMiner/releases/download/0.6.1/VerthashMiner-0.6.1-CUDA11-linux.tar.gz                                                                                                                                                      
+wget https://github.com/vertiond/VerthashMiner/releases/download/0.6.1/VerthashMiner-0.6.1-CUDA11-linux-GLIBC2.27.tar.gz                                                                                                                                                      
 
 tar xvzf VerthashMiner-0.6.1-CUDA11-linux.tar.gz -C . --strip-components=1                                                                                                                                                                                                 
 

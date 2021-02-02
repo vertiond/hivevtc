62f856d20092cd1529ce551aea2416d01408a45e
vertion <35286924+vertiond@users.noreply.github.com>
2021-02-02 17:11:04 -0600

    Update vtc.sh
    CryptoGraphics' 0.6.2 release supports GLIBC 2.27 now.
    
    Allows for new `--gen-verthash-data` line to locally generate verthash.dat.

--- vtc.sh
+++ vtc.sh
@@ -2,18 +2,12 @@ mkdir vtc
 
 cd vtc                                                                                                                                                                                                             
 
-#grab official verthash.dat
-wget https://vtconline.org/downloads/verthash.dat
+wget https://github.com/CryptoGraphics/VerthashMiner/releases/download/0.6.2/VerthashMiner-0.6.2-CUDA11-linux.tar.gz                                                                                                                                                     
 
-#grab vertion's compiled VerthashMiner compatible with GLIBC 2.27 
-wget https://github.com/vertiond/VerthashMiner/releases/download/0.6.1/VerthashMiner-0.6.1-CUDA11-linux-GLIBC2.27.tar.gz                                                                                                                                                      
+tar xvzf VerthashMiner-0.6.2-CUDA11-linux.tar.gz -C . --strip-components=1                                                                                                                                                                                                 
 
-tar xvzf VerthashMiner-0.6.1-CUDA11-linux-GLIBC2.27.tar.gz -C . --strip-components=1                                                                                                                                                                                                 
+miner stop                                                                                                                                                                                                                                                                                                                                                                                                 
 
-miner stop                                                                                                                                                                                                         
-
-chmod +x VerthashMiner                                                                                                                                                                                             
-
-./VerthashMiner -g config.conf                                                                                                                                                                                      
+./VerthashMiner --gen-verthash-data verthash.dat                                                                                                                                                                                     
 
 screen -dmS miner ./VerthashMiner -u vtc1qg9lkhlpcwfm43q73lp7m47qxy8v0cus67zwc6f.example -p x -o stratum+tcp://vtconline.org:9171 --verthash-data verthash.dat --all-cl-devices --all-cu-devices
